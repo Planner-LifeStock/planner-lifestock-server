9aa6f8059aa66593cf2a430b382d9d38c9082415
김혜주 <sueelly@naver.com>
2024-09-22 11:37:47 +0900

    Feat: company findAllByUserId, findById, POST 구현

--- src/main/java/com/lifestockserver/lifestock/company/controller/CompanyController.java
+++ src/main/java/com/lifestockserver/lifestock/company/controller/CompanyController.java
@@ -1,17 +1,45 @@
 package com.lifestockserver.lifestock.company.controller;
 
+import com.lifestockserver.lifestock.company.dto.CompanyCreateDto;
+import com.lifestockserver.lifestock.company.service.CompanyService;
+import com.lifestockserver.lifestock.company.dto.CompanyResponseDto;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
 
-  @PostMapping
-  public ResponseEntity<Company> createCompany(@RequestBody CompanyCreateDto companyCreateDto) {
-    Company company = companyService.createCompany(companyCreateDto);
-    return ResponseEntity.ok(company);
+  private final CompanyService companyService;
+
+  public CompanyController(CompanyService companyService) {
+    this.companyService = companyService;
   }
+
+  @PostMapping
+  public ResponseEntity<CompanyResponseDto> createCompany(@RequestBody CompanyCreateDto companyCreateDto, String userId) {
+    CompanyResponseDto companyResponseDto = companyService.createCompany(companyCreateDto);
+    return ResponseEntity.ok(companyResponseDto);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<CompanyResponseDto>> getCompaniesByUserId(@RequestParam(required = true, value="userId") Long userId) {
+    List<CompanyResponseDto> companyResponseDtos = companyService.findAllByUserId(userId);
+    return ResponseEntity.ok(companyResponseDtos);
+  }
+
+  @GetMapping("/{companyId}")
+  public ResponseEntity<CompanyResponseDto> getCompany(@PathVariable Long companyId) {
+    CompanyResponseDto companyResponseDto = companyService.findById(companyId);
+    return ResponseEntity.ok(companyResponseDto);
+  }
+
 }
--- src/main/java/com/lifestockserver/lifestock/company/service/CompanyService.java
+++ src/main/java/com/lifestockserver/lifestock/company/service/CompanyService.java
@@ -4,29 +4,83 @@ import org.springframework.stereotype.Service;
 
 import jakarta.persistence.EntityNotFoundException;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.lifestockserver.lifestock.company.domain.Company;
-import com.lifestockserver.lifestock.chart.repository.ChartRepository;
+import com.lifestockserver.lifestock.chart.service.ChartService;
 import com.lifestockserver.lifestock.company.repository.CompanyRepository;
+import com.lifestockserver.lifestock.user.service.UserServiceImpl;
+import com.lifestockserver.lifestock.company.dto.CompanyCreateDto;
+import com.lifestockserver.lifestock.company.dto.CompanyResponseDto;
+import com.lifestockserver.lifestock.company.mapper.CompanyMapper;
+import com.lifestockserver.lifestock.user.domain.User;
+import com.lifestockserver.lifestock.file.service.FileService;
+import org.springframework.transaction.annotation.Transactional;
+
+
 
 @Service
 public class CompanyService {
 
-  private final ChartRepository chartRepository;
+  private final ChartService chartService;
   private final CompanyRepository companyRepository;
-  
-  public CompanyService(ChartRepository chartRepository, CompanyRepository companyRepository) {
-    this.chartRepository = chartRepository;
+  private final CompanyMapper companyMapper;
+  private final UserServiceImpl userServiceImpl;
+  private final FileService fileService;
+
+  public CompanyService(ChartService chartService, CompanyRepository companyRepository, CompanyMapper companyMapper, UserServiceImpl userServiceImpl, FileService fileService) {
+    this.chartService = chartService;
     this.companyRepository = companyRepository;
+    this.companyMapper = companyMapper;
+    this.userServiceImpl = userServiceImpl;
+    this.fileService = fileService;
   }
 
-  public Company getCompanyWithStockPrice(Long companyId) {
+  @Transactional
+  public CompanyResponseDto createCompany(CompanyCreateDto companyCreateDto) {
+    User user = userServiceImpl.findMemberById(companyCreateDto.getUserId())
+      .orElseThrow(() -> new EntityNotFoundException("User not found"));
+    companyCreateDto.setUser(user);
 
-    Company company = companyRepository.findById(companyId)
+    // companyCreateDto.logo가 null이면 기본 로고를 설정
+    if (companyCreateDto.getLogo() == null) {
+      companyCreateDto.setLogo(fileService.getDefaultCompanyLogo());
+    }
+
+    Company company = companyMapper.toEntity(companyCreateDto);
+    Company savedCompany = companyRepository.save(company);
+    CompanyResponseDto companyResponseDto = companyMapper.toDto(savedCompany);
+    companyResponseDto.setCurrentStockPrice(savedCompany.getInitialStockPrice());
+    
+    return companyResponseDto;
+  }
+
+  @Transactional(readOnly = true)
+  public List<CompanyResponseDto> findAllByUserId(Long userId) {
+    List<Company> companies = companyRepository.findAllByUserId(userId);
+
+    List<CompanyResponseDto> companyResponseDtos = companies.stream()
+      .map(company -> {
+        CompanyResponseDto companyResponseDto = companyMapper.toDto(company);
+        companyResponseDto.setCurrentStockPrice(chartService.getLatestHighByCompanyId(company.getId()));
+        return companyResponseDto;
+      })
+      .collect(Collectors.toList());
+    return companyResponseDtos;
+  }
+
+  @Transactional(readOnly = true)
+  public CompanyResponseDto findById(Long id) {
+    Company company = companyRepository.findById(id)
       .orElseThrow(() -> new EntityNotFoundException("Company not found"));
 
     // 가장 최근의 high 값을 가져와서 currentStockPrice에 설정
-    chartRepository.findLatestHighByCompanyId(companyId).ifPresent(latestHigh -> company.setCurrentStockPrice(latestHigh));
-
-    return company;
+    Long currentStockPrice = chartService.getLatestHighByCompanyId(id);
+    
+    CompanyResponseDto companyResponseDto = companyMapper.toDto(company);
+    companyResponseDto.setCurrentStockPrice(currentStockPrice);
+    
+    return companyResponseDto;
   }
 }
