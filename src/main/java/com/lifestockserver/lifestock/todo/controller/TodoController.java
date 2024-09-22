1ee7dda7c47788157c64c754bde5a114c68aa95b
김혜주 <sueelly@naver.com>
2024-09-22 21:42:26 +0900

    Feat: Create todo

--- src/main/java/com/lifestockserver/lifestock/todo/controller/TodoController.java
+++ src/main/java/com/lifestockserver/lifestock/todo/controller/TodoController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.time.LocalDate;
 
-import com.lifestockserver.lifestock.todo.domain.Todo;
+import com.lifestockserver.lifestock.todo.dto.TodoCreateDto;
 import com.lifestockserver.lifestock.todo.service.TodoService;
 import com.lifestockserver.lifestock.todo.dto.TodoResponseDto;
 
@@ -38,5 +40,12 @@ public class TodoController {
     @RequestParam(required = true, value = "date") LocalDate date
   ) {
     return ResponseEntity.ok(todoService.getMonthlyTodos(userId, companyId, date));
+  }
+
+  @PostMapping
+  public ResponseEntity<TodoResponseDto> createTodo(
+    @RequestBody TodoCreateDto todoCreateDto
+  ) {
+    return ResponseEntity.ok(todoService.createTodo(todoCreateDto));
   }
 }
--- src/main/java/com/lifestockserver/lifestock/todo/service/TodoService.java
+++ src/main/java/com/lifestockserver/lifestock/todo/service/TodoService.java
@@ -4,7 +4,12 @@ import org.springframework.stereotype.Service;
 import com.lifestockserver.lifestock.todo.repository.TodoRepository;
 import com.lifestockserver.lifestock.todo.dto.TodoResponseDto;
 import com.lifestockserver.lifestock.todo.domain.Todo;
+import com.lifestockserver.lifestock.todo.dto.TodoCreateDto;
 import com.lifestockserver.lifestock.todo.mapper.TodoMapper;
+import com.lifestockserver.lifestock.user.repository.UserRepository;
+import com.lifestockserver.lifestock.company.repository.CompanyRepository;
+import com.lifestockserver.lifestock.company.domain.Company;
+import com.lifestockserver.lifestock.user.domain.User;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -15,10 +20,14 @@ import java.util.Comparator;
 public class TodoService {
   private final TodoRepository todoRepository;
   private final TodoMapper todoMapper;
+  private final UserRepository userRepository;
+  private final CompanyRepository companyRepository;
 
-  public TodoService(TodoRepository todoRepository, TodoMapper todoMapper) {
+  public TodoService(TodoRepository todoRepository, TodoMapper todoMapper, UserRepository userRepository, CompanyRepository companyRepository) {
     this.todoRepository = todoRepository;
     this.todoMapper = todoMapper;
+    this.userRepository = userRepository;
+    this.companyRepository = companyRepository;
   }
 
   public List<TodoResponseDto> getAllTodosByDate(Long userId, Long companyId, LocalDate date) {
@@ -37,9 +46,30 @@ public class TodoService {
     List<TodoResponseDto> todoResponseDtos = todos.stream()
       .map(todo -> todoMapper.toDto(todo))
       .collect(Collectors.toList());
-      
+
     // startDate으로 정렬
     todoResponseDtos.sort(Comparator.comparing(TodoResponseDto::getStartDate));
     return todoResponseDtos;
+  }
+
+  public TodoResponseDto createTodo(TodoCreateDto todoCreateDto) {
+    if (todoCreateDto.getStartDate().isAfter(todoCreateDto.getEndDate())) {
+      throw new RuntimeException("시작일이 종료일보다 클 수 없습니다");
+    }
+
+    User user = userRepository.findById(todoCreateDto.getUserId())
+      .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다"));
+    Company company = companyRepository.findById(todoCreateDto.getCompanyId())
+      .orElseThrow(() -> new RuntimeException("회사를 찾을 수 없습니다"));
+    
+    if (user.getId() != company.getUser().getId()) {
+      throw new RuntimeException("사용자와 회사의 관계가 올바르지 않습니다");
+    }
+
+    Todo todo = todoMapper.toEntity(todoCreateDto, user, company);
+    Todo savedTodo = todoRepository.save(todo);
+
+    // 추후 chart 업데이트되는 로직 추가
+    return todoMapper.toDto(savedTodo);
   }
 }
