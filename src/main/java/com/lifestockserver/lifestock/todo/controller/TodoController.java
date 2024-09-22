c3fd91e04dbf0a91f50c65394df437546d0172be
김혜주 <sueelly@naver.com>
2024-09-22 19:54:23 +0900

    Feat: company id, user id, date에 맞춰 todo 리스트 조회 구현

--- src/main/java/com/lifestockserver/lifestock/todo/controller/TodoController.java
+++ src/main/java/com/lifestockserver/lifestock/todo/controller/TodoController.java
@@ -1,8 +1,17 @@
 package com.lifestockserver.lifestock.todo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.time.LocalDate;
+
+import com.lifestockserver.lifestock.todo.domain.Todo;
 import com.lifestockserver.lifestock.todo.service.TodoService;
+import com.lifestockserver.lifestock.todo.dto.TodoResponseDto;
 
 @RestController
 @RequestMapping("/todo")
@@ -11,5 +20,14 @@ public class TodoController {
 
   public TodoController(TodoService todoService) {
     this.todoService = todoService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<TodoResponseDto>> getAllTodos(
+    @RequestParam(required = true, value = "userId") Long userId, 
+    @RequestParam(required = true, value = "companyId") Long companyId, 
+    @RequestParam(required = true, value = "date") LocalDate date
+  ) {
+    return ResponseEntity.ok(todoService.getAllTodosByDate(userId, companyId, date));
   }
 }
--- src/main/java/com/lifestockserver/lifestock/todo/service/TodoService.java
+++ src/main/java/com/lifestockserver/lifestock/todo/service/TodoService.java
@@ -2,6 +2,12 @@ package com.lifestockserver.lifestock.todo.service;
 
 import org.springframework.stereotype.Service;
 import com.lifestockserver.lifestock.todo.repository.TodoRepository;
+import com.lifestockserver.lifestock.todo.dto.TodoResponseDto;
+import com.lifestockserver.lifestock.todo.domain.Todo;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TodoService {
@@ -9,5 +15,22 @@ public class TodoService {
 
   public TodoService(TodoRepository todoRepository) {
     this.todoRepository = todoRepository;
+  }
+
+  public List<TodoResponseDto> getAllTodosByDate(Long userId, Long companyId, LocalDate date) {
+    List<Todo> todos = todoRepository.findAllByUserIdAndCompanyIdAndDate(userId, companyId, date);
+    
+    List<TodoResponseDto> todoResponseDtos = todos.stream()
+      .map(todo -> TodoResponseDto.builder()
+        .id(todo.getId())
+        .userId(todo.getUser().getId())
+        .companyId(todo.getCompany().getId())
+        .title(todo.getTitle())
+        .description(todo.getDescription())
+        .completed(todo.isCompleted())
+        .done(todo.isDone())
+        .build())
+      .collect(Collectors.toList());
+    return todoResponseDtos;
   }
 }
