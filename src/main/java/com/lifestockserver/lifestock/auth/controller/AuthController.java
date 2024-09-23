99bfc5f4d53ffc54d44337b26b7d07594e42d97d
jigun058 <jigun058@gmail.com>
2024-09-23 18:02:30 +0900

    Fix: 하드코딩 제거, TokenService 인터페이스로 구현

--- src/main/java/com/lifestockserver/lifestock/auth/controller/AuthController.java
+++ src/main/java/com/lifestockserver/lifestock/auth/controller/AuthController.java
@@ -2,10 +2,12 @@ package com.lifestockserver.lifestock.auth.controller;
 
 import com.lifestockserver.lifestock.auth.dto.LoginRequestDto;
 import com.lifestockserver.lifestock.auth.dto.TokenResponseDto;
+import com.lifestockserver.lifestock.auth.exception.InvalidTokenException;
 import com.lifestockserver.lifestock.auth.service.TokenService;
+import com.lifestockserver.lifestock.user.domain.UserRole;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/auth")
@@ -18,20 +20,20 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequest) {
-        String accessToken = tokenService.createAccessToken(loginRequest.getUsername(), "ROLE_USER");
-        return ResponseEntity.ok(accessToken);
+    public ResponseEntity<TokenResponseDto> login(@Valid @RequestBody LoginRequestDto loginRequest) {
+        String accessToken = tokenService.createAccessToken(loginRequest.getUsername(), UserRole.USER.name());
+        return ResponseEntity.ok(new TokenResponseDto(accessToken, null));
     }
 
     @PostMapping("/refresh")
     public ResponseEntity<TokenResponseDto> refresh(@RequestHeader("Authorization") String refreshToken) {
         if (tokenService.validateToken(refreshToken)) {
             String username = tokenService.getUsernameFromToken(refreshToken);
-            String newAccessToken = tokenService.createAccessToken(username, "ROLE_USER");
+            String newAccessToken = tokenService.createAccessToken(username, UserRole.USER.name());
 
             return ResponseEntity.ok(new TokenResponseDto(newAccessToken, refreshToken));
         } else {
-            return ResponseEntity.status(403).body(null);
+            throw new InvalidTokenException("유효하지 않은 리프레시 토큰입니다.");
         }
     }
 }
--- src/main/java/com/lifestockserver/lifestock/auth/dto/LoginRequestDto.java
+++ src/main/java/com/lifestockserver/lifestock/auth/dto/LoginRequestDto.java
@@ -1,9 +1,13 @@
 package com.lifestockserver.lifestock.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequestDto {
+    @NotBlank(message = "아이디는 빈칸일 수 없어요!")
     private String username;
+
+    @NotBlank(message = "비밀번호는 빈칸일 수 없어요!")
     private String password;
 }
