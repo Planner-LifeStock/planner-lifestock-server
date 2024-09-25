58e0060cc650439ab8579fa278fe8436eb53fa8d
김혜주 <sueelly@naver.com>
2024-09-25 19:08:54 +0900

    Error: 리스트 반환하는 거 모두 에러터진다 망할~~

--- src/main/java/com/lifestockserver/lifestock/chart/dto/CompanyChartListResponseDto.java
+++ src/main/java/com/lifestockserver/lifestock/chart/dto/CompanyChartListResponseDto.java
@@ -1,19 +1,11 @@
 package com.lifestockserver.lifestock.chart.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.AllArgsConstructor;
-
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class CompanyChartListResponseDto extends ChartListResponseDto {
-  private Long companyId;
-  private List<CompanyChartElementResponseDto> chartList;
+
+abstract class CompanyChartListResponseDto extends ChartListResponseDto {
+  protected Long companyId;
+  protected List<CompanyChartElementResponseDto> chartList;
 }
+
+
