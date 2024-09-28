c23b5ef56ea9bc21255f16dc4164978bbdbb5189
박도현 <qkrehgus02@naver.com>
2024-09-28 16:50:35 +0900

    TodoServiceTest 테스트코드 작성됨

--- src/test/java/com/lifestockserver/lifestock/todo/TodoServiceTest.java
+++ src/test/java/com/lifestockserver/lifestock/todo/TodoServiceTest.java
@@ -3,6 +3,7 @@ package com.lifestockserver.lifestock.todo;
 import com.lifestockserver.lifestock.company.domain.Company;
 import com.lifestockserver.lifestock.company.repository.CompanyRepository;
 import com.lifestockserver.lifestock.todo.domain.Todo;
+import com.lifestockserver.lifestock.todo.domain.enums.TodoLevel;
 import com.lifestockserver.lifestock.todo.dto.TodoCreateDto;
 import com.lifestockserver.lifestock.todo.dto.TodoResponseDto;
 import com.lifestockserver.lifestock.todo.mapper.TodoMapper;
@@ -17,7 +18,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -105,5 +110,138 @@ public class TodoServiceTest {
         assertNotNull(result);
         assertEquals(1L, result.getId());
         verify(todoRepository, times(1)).save(any(Todo.class));
+    }
+
+    @Test
+    void  testUpdateTodoCompleted(){
+
+        //Given
+        Long todoId = 1L;
+        User user = new User();
+        user.setId(1L);
+
+        Company company = Company.builder()
+                .id(1L)
+                .user(user)
+                .build();
+
+        Todo todo = Todo.builder()
+                .id(1L)
+                .user(user)
+                .company(company)
+                .completed(false)
+                .build();
+
+        when(todoRepository.findByIdAndDeletedAtIsNull(todoId)).thenReturn(todo);
+        when(todoRepository.save(any(Todo.class))).thenReturn(todo);
+        when(todoMapper.toDto(any(Todo.class))).thenReturn(TodoResponseDto.builder()
+                .id(todoId)
+                .completed(true)  // 완료 상태로 변경된 상태를 반환
+                .build());
+
+        //when
+        TodoResponseDto result = todoService.updateTodoCompleted(todoId);
+
+        //Then
+        assertNotNull(result);
+        assertEquals(todoId, result.getId());
+        assertTrue(result.isCompleted());  // 완료 상태가 true인지 확인
+        verify(todoRepository, times(1)).save(todo);
+    }
+
+    @Test
+    void testDeleteTodo(){
+        //Given
+        Long todoId = 1L;
+        String deleteReason = "테스트 삭제 사유";
+        User user = new User();
+        user.setId(1L);
+
+        Company company = Company.builder()
+                .id(1L)
+                .user(user)
+                .build();
+
+        Todo todo = Todo.builder()
+                .id(1L)
+                .title("테스트 Todo")
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now().plusDays(1))
+                .days(EnumSet.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY))
+                .build();
+
+        when(todoRepository.findByIdAndDeletedAtIsNull(todoId)).thenReturn(todo);
+
+        //when
+        todoService.deleteTodo(todoId, deleteReason);
+
+        //then
+        assertNotNull(todo.getDeletedAt());
+        assertEquals(deleteReason, todo.getDeletedReason());
+        verify(todoRepository, times(1)).save(todo);
+    }
+
+    @Test
+    void testGetAllTodosByDate(){
+        Long userId = 1L;
+        Long companyId = 1L;
+        LocalDate date = LocalDate.now().with(DayOfWeek.MONDAY);
+
+        Todo todo = Todo.builder()
+                .id(1L)
+                .title("테스트 Todo")
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now().plusDays(1))
+                .days(EnumSet.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY))
+                .build();
+
+        todoResponseDto = TodoResponseDto.builder()
+                .id(1L)
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now().plusDays(1))
+                .build();
+
+        List<Todo> todos = Arrays.asList(todo);
+        when(todoRepository.findAllByUserIdAndCompanyIdAndDate(userId, companyId, date)).thenReturn(todos);
+        when(todoMapper.toDto(any(Todo.class))).thenReturn(todoResponseDto);
+
+        List<TodoResponseDto> result = todoService.getAllTodosByDate(userId, companyId, date);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(todoResponseDto.getId(), result.get(0).getId());
+
+        verify(todoRepository, times(1)).findAllByUserIdAndCompanyIdAndDate(userId, companyId, date);
+        verify(todoMapper, times(1)).toDto(todo);
+    }
+
+    @Test
+    void testGetMonthlyTodos(){
+        Long userId = 1L;
+        Long companyId = 1L;
+        LocalDate date = LocalDate.now();
+
+        Todo todo = Todo.builder()
+                .id(1L)
+                .title("테스트 Todo")
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now().plusDays(1))
+                .days(EnumSet.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY))
+                .build();
+
+        List<Todo> todos = Arrays.asList(todo);
+
+        when(todoRepository.findAllByUserIdAndCompanyIdAndMonth(userId, companyId, date)).thenReturn(todos);
+        when(todoMapper.toDto(any(Todo.class))).thenReturn(todoResponseDto);
+
+        List<TodoResponseDto> result = todoService.getMonthlyTodos(userId, companyId, date);
+
+        // 결과 확인
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(todoResponseDto.getId(), result.get(0).getId());
+
+        verify(todoRepository, times(1)).findAllByUserIdAndCompanyIdAndMonth(userId, companyId, date);
+        verify(todoMapper, times(1)).toDto(todo);
     }
 }
