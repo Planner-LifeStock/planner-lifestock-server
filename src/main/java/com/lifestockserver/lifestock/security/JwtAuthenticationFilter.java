c1a6aeb6d5b5935ac6ef2134899f4948065b77ac
김혜주 <sueelly@naver.com>
2024-10-28 17:49:43 +0900

    fix: security filter off, jwt filter만 작동

--- src/main/java/com/lifestockserver/lifestock/security/JwtAuthenticationFilter.java
+++ src/main/java/com/lifestockserver/lifestock/security/JwtAuthenticationFilter.java
@@ -29,6 +29,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
+        if (shouldNotFilter(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String token = extractToken(request);
 
         // 토큰이 유효하면 SecurityContext에 저장
@@ -37,18 +42,33 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
 
+        if (token == null) {
+            throw new ServletException("토큰이 유효하지 않습니다.");
+        }
+        
         // 다음 필터로 요청 전달
         filterChain.doFilter(request, response);
     }
 
+    @Override
+    protected boolean shouldNotFilter(@NonNull HttpServletRequest request) throws ServletException {
+        return request.getRequestURI().equals("/auth/login") ||
+                request.getRequestURI().equals("/user/register");
+    }
+
     private String extractToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
+
         if (bearerToken == null) {
             logger.warn("Authorization 헤더가 존재하지 않습니다.");
             return null;
         }
         if (!bearerToken.startsWith("Bearer ")) {
             logger.warn("Authorization 헤더의 형식이 올바르지 않습니다. 토큰이 'Bearer '로 시작해야 합니다.");
+            return null;
+        }
+        if (!authServiceProvider.get().validateToken(bearerToken.substring(7))) {
+            logger.warn("유효하지 않은 토큰입니다.");
             return null;
         }
 
