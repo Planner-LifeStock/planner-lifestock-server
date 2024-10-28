f31ea716c493f51639361bb17102359938be1680
김혜주 <sueelly@naver.com>
2024-10-28 16:05:07 +0900

    feat: jwt access token에 id 정보 추가

--- src/main/java/com/lifestockserver/lifestock/auth/service/AuthService.java
+++ src/main/java/com/lifestockserver/lifestock/auth/service/AuthService.java
@@ -6,7 +6,7 @@ import com.lifestockserver.lifestock.user.domain.UserRole;
 import org.springframework.security.core.Authentication;
 
 public interface AuthService {
-    String createAccessToken(String username, UserRole role);
+    String createAccessToken(String username, Long userId, UserRole role);
     String createRefreshToken(String username);
     String getUsernameFromToken(String token);
     boolean validateToken(String token);
