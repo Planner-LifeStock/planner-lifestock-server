9770e42f54d9f6421d96d9d10093d84d659f1620
dohyeon <qkrehgus02@naver.com>
2024-11-04 22:02:34 +0900

    schedular

--- src/main/java/com/lifestockserver/lifestock/config/UserAssetUpdateJobConfig.java
+++ src/main/java/com/lifestockserver/lifestock/config/UserAssetUpdateJobConfig.java
@@ -6,10 +6,13 @@ import com.lifestockserver.lifestock.user.domain.User;
 import com.lifestockserver.lifestock.user.dto.UserResponseDto;
 import com.lifestockserver.lifestock.user.service.UserServiceImpl;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,7 +49,38 @@ public class UserAssetUpdateJobConfig {
         return new ListItemReader<>(userIds);
     }
 
+    @Bean
+    public ItemProcessor<Long, Long> userItemProcessor(){
+        return userId -> {
+            Long totalAssets = chartService.getTotalStockPriceByUserId(userId); // 차트 서비스를 통해 유저의 총자산 반환
+            rankService.updateUserAsset(userId, totalAssets);
+            return userId;
+        };
+    }
 
+    @Bean
+    public ItemWriter<Long> userItemWriter(){
+        return userIds -> {
+            System.out.println("Processed" + userIds.size() + "users' assets.");
+        };
+    }
+
+    @Bean
+    public Step updateUserAssetStep(){
+        return new StepBuilder("updateUserAssetStep", jobRepository)
+                .<Long, Long>chunk(10, transactionManager)
+                .reader(userItemReader())
+                .processor(userItemProcessor())
+                .writer(userItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job updateUserAssetJob(){
+        return new JobBuilder("updateUserAssetJob", jobRepository)
+                .start(updateUserAssetStep())
+                .build();
+    }
     /*
     @Bean
     public Job updateUserAssetJob(){
