ca2386c9dd3760adca075ed38489353832cc75ae
jigun058 <jigun058@gmail.com>
2024-11-10 19:21:35 +0900

    Fix: 총자산 계산 수정

--- src/main/java/com/lifestockserver/lifestock/chart/repository/ChartRepository.java
+++ src/main/java/com/lifestockserver/lifestock/chart/repository/ChartRepository.java
@@ -67,7 +67,7 @@ public interface ChartRepository extends JpaRepository<Chart, Long> {
 
        @Query("SELECT SUM(c.close * c.company.initialStockQuantity) FROM Chart c " +
               "WHERE c.user.id = :userId " +
-              "AND c.date = (SELECT MAX(c2.date) FROM Chart c2 WHERE c2.company.id = c.company.id AND c2.user.id = :userId)")
+              "AND c.date = (SELECT MAX(C2.createdAt) FROM Chart c2 WHERE c2.company.id = c.company.id AND c2.user.id = :userId)")
        Long getTotalStockPriceByUserId(Long userId);
 
 }
