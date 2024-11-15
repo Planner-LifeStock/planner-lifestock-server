14d8fa1acee409662b2ddd27437322f954210386
taegwonbrew <jigun058@gmail.com>
2024-11-15 20:45:50 +0900

    Fix: 주간 완료 여부 오류 수정

--- src/main/generated/com/lifestockserver/lifestock/user/mapper/UserMapperImpl.java
+++ src/main/generated/com/lifestockserver/lifestock/user/mapper/UserMapperImpl.java
@@ -3,16 +3,13 @@ package com.lifestockserver.lifestock.user.mapper;
 import com.lifestockserver.lifestock.user.domain.User;
 import com.lifestockserver.lifestock.user.dto.UserCreateDto;
 import com.lifestockserver.lifestock.user.dto.UserResponseDto;
-import com.lifestockserver.lifestock.user.dto.UserUpdateDto;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
-import com.lifestockserver.lifestock.user.domain.UserStatus;
-import com.lifestockserver.lifestock.user.domain.UserRole;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-13T23:06:24+0900",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Eclipse Adoptium)"
+    date = "2024-11-15T14:36:41+0900",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.11 (Oracle Corporation)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
@@ -23,13 +20,14 @@ public class UserMapperImpl implements UserMapper {
             return null;
         }
 
-        User.UserBuilder user = User.builder()
-            .username(userCreateDto.getUsername())
-            .password(userCreateDto.getPassword())
-            .realName(userCreateDto.getRealName())
-            .displayName(userCreateDto.getDisplayName())
-            .email(userCreateDto.getEmail())
-            .phoneNumber(userCreateDto.getPhoneNumber());
+        User.UserBuilder user = User.builder();
+
+        user.username( userCreateDto.getUsername() );
+        user.password( userCreateDto.getPassword() );
+        user.realName( userCreateDto.getRealName() );
+        user.displayName( userCreateDto.getDisplayName() );
+        user.email( userCreateDto.getEmail() );
+        user.phoneNumber( userCreateDto.getPhoneNumber() );
 
         return user.build();
     }
@@ -40,16 +38,17 @@ public class UserMapperImpl implements UserMapper {
             return null;
         }
 
-        UserResponseDto.UserResponseDtoBuilder userResponseDto = UserResponseDto.builder()
-            .status(user.getStatus())
-            .id(user.getId())
-            .username(user.getUsername())
-            .realName(user.getRealName())
-            .email(user.getEmail())
-            .displayName(user.getDisplayName())
-            .phoneNumber(user.getPhoneNumber())
-            .role(user.getRole())
-            .asset(user.getAsset());
+        UserResponseDto.UserResponseDtoBuilder userResponseDto = UserResponseDto.builder();
+
+        userResponseDto.id( user.getId() );
+        userResponseDto.username( user.getUsername() );
+        userResponseDto.realName( user.getRealName() );
+        userResponseDto.email( user.getEmail() );
+        userResponseDto.displayName( user.getDisplayName() );
+        userResponseDto.phoneNumber( user.getPhoneNumber() );
+        userResponseDto.status( user.getStatus() );
+        userResponseDto.role( user.getRole() );
+        userResponseDto.asset( user.getAsset() );
 
         return userResponseDto.build();
     }
